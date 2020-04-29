cbb0967c17f0da0b99c0544d3133e79b84643ef0
FokaKefir <babosdavid8@gmail.com>
2020-04-29 14:05:47 +0300

    Adding the TreeNode.

---
+++ src/com/company/model/TreeNode.java
@@ -0,0 +1,57 @@
+package com.company.model;
+
+public class TreeNode {
+
+    // region 1. Init and Decl
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    // endregion
+
+    // region 2. Constructor
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    // endregion
+
+    // region 3. Getters and Setters
+
+    public int getVal() {
+        return val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    // endregion
+
+}
